ff1d90ac861620a1fe7cb844a87f4de83ece7d68
Vega <bellagold562@gmail.com>
2022-05-17 10:32:19 -0700

    background

---
+++ Sem2_Final/Assets/Scripts/RepeatBack.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBack : MonoBehaviour
+{
+    private Vector2 startPos;
+    public float speed = 20;
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        transform.position = new Vector3((pos.x - Time.deltaTime * speed) % (0.32f * 21.914f), 0.03f, 0);
+    }
+
+}
