20e73229bc86a4b82297d9fcafe405d047fef26c
Vega <bellagold562@gmail.com>
2022-05-17 10:37:49 -0700

    game controller script

---
+++ Sem2_Final/Assets/Scripts/GameController.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+    public static GameControl instance;
+    //public GameObject gameOverText;
+    //public bool gameOver = false;
+    public float scrollSpeed = -1.5f;
+    //public Text scoreText;
+
+    //private int score = 0;
+
+    // public AudioClip scoredClip;
+    // AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+  /* public void BirdScored ()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+score++;
+scoreText.text = "Score:" + score.ToString();
+
+     //   PlaySound(scoredClip);
+    }
+
+  //  public void SpaceshipCrash()
+    {
+    gameOverText.SetActive(true);
+    gameOver = true;
+}
+
+//  public void PlaySound(AudioClip clip)
+{
+    audioSource.PlayOneShot(clip);
+}
+  */
+}
+
+
